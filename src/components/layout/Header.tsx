4fced5c0a06dcfcfc34a4e45c50a3da0b7f50f7f
HELAL15 <1ahmedhelal1@gmail.com>
2025-05-14 16:23:37 +0300

    navbar logo bold

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     return (
         <header className='bg-background/95 supports-[backdrop-filter]:bg-background/60 border-b-secondary text-foreground sticky top-0 z-50 border-b py-3 antialiased backdrop-blur'>
             <div className='container flex w-full items-center justify-between gap-6'>
-                <Link href={'/'} className='text-xl md:text-2xl lg:text-3xl'>
+                <Link href={'/'} className='text-xl font-bold md:text-2xl lg:text-3xl'>
                     HELAL
                 </Link>
                 <nav className='hidden lg:flex'>
