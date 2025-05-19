28f9f55a09242211882af630663ad4a13ccbee90
HELAL15 <1ahmedhelal1@gmail.com>
2025-05-19 13:43:37 +0300

    add bluredcircle

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 import LoginForm from '@/components/login/LoginForm';
+import BluredCircles from '@/components/ui/BluredCircles';
 
 export const metadata: Metadata = {
     title: 'login - websiteName'
@@ -10,6 +11,7 @@ const Page = () => {
     return (
         <>
             <main>
+                <BluredCircles />
                 <section className='relative z-10 my-12 lg:my-20'>
                     <div className='container grid h-full place-items-center space-y-8'>
                         <h1 className='text-center text-3xl font-bold'>login</h1>
